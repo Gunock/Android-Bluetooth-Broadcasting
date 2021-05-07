02be16ac7d808dacfd88065c6d0f7e71a4d3b89d
Gunock <asz.czyk@gmail.com>
2021-05-07 12:05:26 +0200

    Initial dev commit

---
+++ clientApp/src/main/java/pl/gunock/bluetoothexample/client/MainActivity.kt
@@ -0,0 +1,157 @@
+package pl.gunock.client
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private companion object {
+        const val TAG = "MainActivity"
+        const val BT_PERMISSION = 1
+        const val REQUEST_ENABLE_BT = 2
+
+        val SERVICE_UUID = UUID(1, 1)
+    }
+
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+
+    private var mConnectedDevice: BluetoothDevice? = null
+
+    private val mSocket: BluetoothSocket? by lazy(LazyThreadSafetyMode.NONE) {
+        mConnectedDevice?.createRfcommSocketToServiceRecord(SERVICE_UUID)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val permissions: MutableList<String> = mutableListOf()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val isPermissionGranted = ContextCompat.checkSelfPermission(
+                baseContext,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+
+            if (isPermissionGranted != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+        }
+
+        if (permissions.isEmpty()) {
+            initializeButtons()
+        } else {
+            requestPermissions(permissions.toTypedArray(), BT_PERMISSION)
+        }
+    }
+
+    private fun initializeButtons() {
+        val bluetoothManager =
+            baseContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = bluetoothManager.adapter
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(
+                baseContext,
+                "Oops! It looks like your device doesn't have bluetooth!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        if (mBluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+        val pairedDevices: Set<BluetoothDevice>? = mBluetoothAdapter?.bondedDevices
+        pairedDevices?.forEach { device ->
+            val deviceName = device.name
+            val deviceHardwareAddress = device.address
+            Log.d(TAG, "DevName: $deviceName")
+            Log.d(TAG, "DevMAC: $deviceHardwareAddress")
+        }
+        mConnectedDevice = pairedDevices?.first { device ->
+            listOf("xperia", "oneplus").any {
+//            listOf("xperia", "galaxy", "oneplus").any {
+                device.name.contains(it, true)
+            }
+        }
+
+        findViewById<Button>(R.id.btn_client).setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.i(TAG, "Started listening")
+                connectToServer()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.i(TAG, "Request permission result: $requestCode")
+        if (requestCode != BT_PERMISSION) {
+            return
+        }
+
+        if (grantResults.any { it != PackageManager.PERMISSION_GRANTED }) {
+            Log.i(TAG, "Permission not granted")
+            return
+        }
+
+        initializeButtons()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_CANCELED) {
+            Log.d(TAG, "User refused REQUEST_ENABLE_BT")
+        }
+    }
+
+    private fun manageMyConnectedSocket(bluetoothSocket: BluetoothSocket) {
+
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    private suspend fun connectToServer() {
+        mBluetoothAdapter?.cancelDiscovery()
+
+        mSocket?.use { socket ->
+            socket.connect()
+            if (socket.isConnected) {
+                Log.i(TAG, "Connected")
+            }
+
+            delay(1000)
+            val available = socket.inputStream.available()
+            Log.i(TAG, "Available data: $available")
+
+            val buffer = ByteArray(available)
+            socket.inputStream.read(buffer)
+            val text = buffer.decodeToString()
+            Log.i(TAG, "Received message '$text'")
+
+            manageMyConnectedSocket(socket)
+            socket.close()
+        }
+    }
+}
---
+++ serverApp/src/main/java/pl/gunock/bluetoothexample/server/MainActivity.kt
@@ -0,0 +1,163 @@
+package com.example.bluetoothexample
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+    private companion object {
+        const val TAG = "MainActivity"
+
+        const val BT_PERMISSION = 1
+
+        const val REQUEST_ENABLE_BT = 1
+        const val REQUEST_DISCOVERABLE = 2
+
+        const val SERVICE_NAME = "SERVICE"
+        val SERVICE_UUID = UUID(1, 1)
+    }
+
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+
+    private val mSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
+        mBluetoothAdapter?.listenUsingInsecureRfcommWithServiceRecord(SERVICE_NAME, SERVICE_UUID)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val permissions: MutableList<String> = mutableListOf()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val isPermissionGranted = ContextCompat.checkSelfPermission(
+                baseContext,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+
+            if (isPermissionGranted != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+        }
+        if (permissions.isEmpty()) {
+            initializeButtons()
+        } else {
+            requestPermissions(permissions.toTypedArray(), BT_PERMISSION)
+        }
+    }
+
+    private fun initializeButtons() {
+        val bluetoothManager =
+            baseContext.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = bluetoothManager.adapter
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(
+                baseContext,
+                "Oops! It looks like your device doesn't have bluetooth!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        if (mBluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+        CoroutineScope(Dispatchers.Main).launch {
+            val discoverableIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+            intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 600)
+            startActivityForResult(discoverableIntent, REQUEST_DISCOVERABLE)
+        }
+
+        findViewById<Button>(R.id.btn_server).setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                Log.i(TAG, "Started listening")
+                acceptConnection()
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.i(TAG, "Request permission result: $requestCode")
+        if (requestCode != BT_PERMISSION) {
+            return
+        }
+
+        if (grantResults.any { it != PackageManager.PERMISSION_GRANTED }) {
+            Log.i(TAG, "Permission not granted")
+            return
+        }
+
+        initializeButtons()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_CANCELED) {
+            Log.d(TAG, "User refused REQUEST_ENABLE_BT")
+        }
+
+        if (requestCode == REQUEST_DISCOVERABLE && resultCode == RESULT_CANCELED) {
+            Log.d(TAG, "User refused REQUEST_DISCOVERABLE")
+        }
+
+    }
+
+    private fun manageMyConnectedSocket(bluetoothSocket: BluetoothSocket) {
+
+    }
+
+    @Suppress("BlockingMethodInNonBlockingContext")
+    suspend fun acceptConnection() {
+        var shouldLoop = true
+        while (shouldLoop) {
+            val socket: BluetoothSocket? = try {
+                mSocket?.accept()
+            } catch (e: IOException) {
+                Log.e(TAG, "Socket's accept() method failed", e)
+                shouldLoop = false
+                null
+            }
+            socket?.also {
+                Log.i(TAG, "Connection accepted")
+                manageMyConnectedSocket(it)
+
+                val text = "Hello there!"
+                socket.outputStream.write(text.toByteArray())
+                socket.outputStream.flush()
+                Log.i(TAG, "Sent message '$text'")
+
+                delay(5000)
+
+                socket.close()
+                shouldLoop = false
+            }
+        }
+    }
+}
+
